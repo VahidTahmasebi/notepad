df3a2a344ba188841ea2c0bb8795022d51e7bdff
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-27 17:28:04 +0330

    Add static dom

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,3 +1,20 @@
 export default class NotesView {
-    constructor()
+  constructor(root) {
+    this.root = root;
+
+    this.root.innerHTML = `
+    <nav class="notes__sidebar">
+        <div class="notes__logo">Note APP</div>
+        <div class="notes__list">
+        </div>
+        <button class="notes__add">New Note</button>
+      </nav>
+      <main class="notes__preview">
+        <input
+          type="text"
+          class="notes__title"
+          placeholder="Subject..."/>
+        <textarea name="" class="notes__body" placeholder="notes..."></textarea></main>
+    `;
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
+
+const app = document.getElementById("app");
+
+const view = new NotesView(app);
